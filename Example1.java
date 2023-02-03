ddefd2ae86c10f4d9a16c079a60e85e3d86d26d1
Brandon Brzuszkiewicz <bbrzuszk@psd202.org>
2023-02-03 07:18:39 -0600

    Adding a random java file
    Array List 1 Example

---
+++ Example1.java
@@ -0,0 +1,12 @@
+package examplesLesson1;
+
+import java.util.ArrayList;
+
+public class Example1 {
+	public static void main(String[] args) {
+		ArrayList names = new ArrayList();
+		
+		System.out.println("names contains " + names);
+		System.out.println();
+	}
+}
